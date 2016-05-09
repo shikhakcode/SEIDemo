e9d6b957752bcf2b2f1c084a582d51eb12dd1cbc
shikhakcode <dkatiyar@DEKATIYA.in.ibm.com>
2016-05-09 12:58:56 -0400

    removed webapp folder
    removed webapp folder

--- src/main/java/com/demo/DeDup.java
+++ src/main/java/com/demo/DeDup.java
@@ -9,121 +9,125 @@ import java.util.Set;
  *
  */
 public class DeDup {
-	
-    /**
-     * Positive : Performance will be good with small size array 
-     * Negative : for large size array directly effecting system performance because of internally position shifting as well as may not return expected result 
-     * 
-     * @param arr 
-     * @return Array with Unique elements.
-     */
-    
-    public int[] eliminateArrayDuplicates(int[] inputArray) {    	 
-    	int count=0;
-    	
-        if(inputArray != null){
-        	 count = inputArray.length;
-        	 
-        	 for (int i = 0; i < count; i++) {
-             	
-             	// Loop for comparing value with next value and so on
-                 for (int j = i + 1; j < count; j++) { 
-                     if (inputArray[i] == inputArray[j]) {                  
-                         int shiftLeft = j;
-                         
-                       //left shifting in array after finding duplicate 
-                         for (int k = j+1; k < count; k++, shiftLeft++) { 
-                        	 inputArray[shiftLeft] = inputArray[k];
-                         }
-                         count--;
-                         j--;
-                     }
-                 }
-             }
-        }
-       
-        // Copy unique value from temp into result
-        int[] resultArray = new int[count]; 
-        for(int i = 0; i < count; i++){
-        	resultArray[i] = inputArray[i];
-        }
-        
-        return resultArray;
-    }
-    
-    /**
-     * Positive : No much changes in internal structure of original array as well as maintaining original insertion order. 
-     * Negative : Default initialization of result array with input array size and manipulation in size of result array 
-     * 
-     * @param arr
-     * @return Array with Unique elements.
-     */
-    
+
+	/**
+	 * Positive : Performance will be good with small size array. Negative : for
+	 * large size array directly effecting system performance because of
+	 * internally position shifting.
+	 * 
+	 * @param arr
+	 * @return Array with Unique elements.
+	 */
+
+	public int[] eliminateArrayDuplicates(int[] inputArray) {
+		int count = 0;
+
+		if (inputArray != null) {
+			count = inputArray.length;
+
+			for (int i = 0; i < count; i++) {
+
+				// Loop for comparing value with next value and so on
+				for (int j = i + 1; j < count; j++) {
+					if (inputArray[i] == inputArray[j]) {
+						int shiftLeft = j;
+
+						// left shifting in array after finding duplicate
+						for (int k = j + 1; k < count; k++, shiftLeft++) {
+							inputArray[shiftLeft] = inputArray[k];
+						}
+						count--;
+						j--;
+					}
+				}
+			}
+		}
+
+		// Copy unique value from temp into result
+		int[] resultArray = new int[count];
+		for (int i = 0; i < count; i++) {
+			resultArray[i] = inputArray[i];
+		}
+
+		return resultArray;
+	}
+
+	/**
+	 * Positive : No much changes in internal structure of original array as
+	 * well as maintaining original insertion order. Negative : Default
+	 * initialization of result array with input array size and manipulation in
+	 * size of result array
+	 * 
+	 * @param arr
+	 * @return Array with Unique elements.
+	 */
+
 	public int[] removeArrayDuplicates(int[] inputArray) {
- 
+
 		int count = 0;
 		int[] tempArray = null;
 		int[] resultArray = null;
-		if(inputArray != null){
-		    // Temp array
-		    tempArray = new int[inputArray.length]; 			
+		if (inputArray != null) {
+			// Temp array
+			tempArray = new int[inputArray.length];
 
-			for (int i = 0; i < inputArray.length; i++) { 
+			for (int i = 0; i < inputArray.length; i++) {
 				boolean exists = false;
-				
+
 				// Loop to find duplicate existences in temp array
-				for (int j = 0; j < tempArray.length; j++) { 
+				for (int j = 0; j < tempArray.length; j++) {
 					if (tempArray[j] == inputArray[i]) {
 						exists = true;
 					}
 				}
-				
+
 				// Set unique value in temp array
 				if (!exists) {
-					tempArray[count] = inputArray[i]; 
+					tempArray[count] = inputArray[i];
 					count++;
 				}
 			}
-			
+
 			// Copy unique value from temp into result
-			resultArray = new int[count]; 
+			resultArray = new int[count];
 			System.arraycopy(tempArray, 0, resultArray, 0, count);
-		 }
-		
+		}
+
 		return resultArray;
 	}
-    
-	 /**
-     * 
-     * Positive : Highly efficient with large size of array as time complexity with Set is o(N)  
-     * Negative : does not guarantee order of elements returned. 
-     * 
-     * @param arr 
-     * @return Array with Unique elements.
-     */
-	
-    public int[] eliminateDuplicates(int[] inputArray) {
-    	
-    	int[] resultArray = null;
-    	if(inputArray != null){
+
+	/**
+	 * 
+	 * Positive : Highly efficient with large size of array as time complexity
+	 * with Set is o(N). Negative : does not guarantee order of elements
+	 * returned.
+	 * 
+	 * @param arr
+	 * @return Array with Unique elements.
+	 */
+
+	public int[] eliminateDuplicates(int[] inputArray) {
+
+		int[] resultArray = null;
+		if (inputArray != null) {
 			Set<Integer> myset = new HashSet<Integer>();
-	
-	    	//remove duplicate
-		    for( int i = 0; i < inputArray.length; i++ ){
-		        myset.add(inputArray[i]);
-		    }
-	
-		    //Copy unique values into the result.
-		    resultArray = new int[myset.size()];
-		    Iterator itr = myset.iterator();
-		    int count = 0;
-		    while( itr.hasNext() ){
-		    	resultArray[count] = ((Integer)itr.next()).intValue();
-		        count++;
-		    }
-    	}    	
-	    
-	    return resultArray;
+
+			// remove duplicate
+			for (int i = 0; i < inputArray.length; i++) {
+				myset.add(inputArray[i]);
+			}
+
+			// Copy unique values into the result.
+			resultArray = new int[myset.size()];
+			Iterator itr = myset.iterator();
+			int count = 0;
+			while (itr.hasNext()) {
+				resultArray[count] = ((Integer) itr.next()).intValue();
+				count++;
+			}
+		}
+
+		return resultArray;
 	}
 
 }
--- src/test/java/com/demo/DeDupTest.java
+++ src/test/java/com/demo/DeDupTest.java
@@ -6,67 +6,40 @@ import static org.junit.Assert.*;
 import com.demo.DeDup;
 
 public class DeDupTest{
+
+	public DeDup obj = new DeDup();
+
+	public int[] randomIntegers = { 1, 2, 34, 34, 25, 1, 45, 3, 26, 85, 4, 34,
+			86, 25, 43, 2, 1, 10000, 11, 16, 19, 1, 18, 4, 9, 3, 20, 17, 8, 15,
+			6, 2, 5, 10, 14, 12, 13, 7, 8, 9, 1, 2, 15, 12, 18, 10, 14, 20, 17,
+			16, 3, 6, 19, 13, 5, 11, 4, 7, 19, 16, 5, 9, 12, 3, 20, 7, 15, 17,
+			10, 6, 1, 8, 18, 4, 14, 13, 2, 11 };
+
+	public int[] orderedIntegers = { 1, 2, 34, 25, 45, 3, 26, 85, 4, 86, 43, 10000,
+			11, 16, 19, 18, 9, 20, 17, 8, 15, 6, 5, 10, 14, 12, 13, 7 };
 	
-	public DeDup obj = new DeDup(); 	
-    public int[] randomIntegers = {1,2,34,34,25,1,45,3,26,85,4,34,86,25,43,2,1,10000,11,16,19,1,18,
-            4,9,3,20,17,8,15,6,2,5,10,14,12,13,7,8,9,1,2,15,12,18,10,14,20,17,16,3,6,19,
-            13,5,11,4,7,19,16,5,9,12,3,20,7,15,17,10,6,1,8,18,4,14,13,2,11};
+	public int[] unorderedIntegers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+			13, 14, 15, 85, 17, 16, 19, 86, 18, 20, 25, 26, 34, 43, 10000, 45 };
 
 	@Test
-    public void testEliminateArrayDuplicates() throws Exception {		
-	 
-	    int[] result = obj.eliminateArrayDuplicates(randomIntegers);
-	    System.out.print( "Result array1 size: ");
-	    if(result != null){
-	    	System.out.println(result.length);
-	    	
-	    	// iterate to return unique values
-		    for (Integer value : result) {
-		        if (value != null) {
-		            System.out.print( value+",");
-		        }
-		    }
-	    }
-	    
-	    assertNotNull(result);
-    }
-	
-	@Test
-    public void testRemoveArrayDuplicates() throws Exception {
+	public void testEliminateArrayDuplicates() throws Exception {
 
-	    int[] result = obj.removeArrayDuplicates(randomIntegers);	
-	    System.out.println( "Result array2 size: ");
-	    if(result != null){
-	    	System.out.println(result.length);
-	    	
-	    	// iterate to return unique values
-		    for (Integer value : result) {
-		        if (value != null) {
-		            System.out.print( value+",");
-		        }
-		    }
-	    }
-	    
-	    assertNotNull(result);
-    }
-	
+		int[] result = obj.eliminateArrayDuplicates(randomIntegers);
+		assertArrayEquals(orderedIntegers, result);
+	}
+
 	@Test
-    public void testEliminateDuplicates() throws Exception {
-	 
-	    int[] result = obj.eliminateDuplicates(randomIntegers);
-	    System.out.println( "Result array3 size: ");
-	    if(result != null){
-	    	System.out.println(result.length);
-	    	
-	    	// iterate to return unique values
-		    for (Integer value : result) {
-		        if (value != null) {
-		            System.out.print( value+",");
-		        }
-		    }
-	    }
-	    
-	    assertNotNull(result);
-    }
+	public void testRemoveArrayDuplicates() throws Exception {
+
+		int[] result = obj.removeArrayDuplicates(randomIntegers);
+		assertArrayEquals(orderedIntegers, result);
+	}
+
+	@Test
+	public void testEliminateDuplicates() throws Exception {
+
+		int[] result = obj.eliminateDuplicates(randomIntegers);
+		assertArrayEquals(unorderedIntegers, result);
+	}
 
 }
